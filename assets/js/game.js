4f330d1e0d92f54ab2d133604afef25c573fd855
amiga909 <lfo@gmx.ch>
2022-05-26 14:16:33 +0200

    game start button

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,9 +2,27 @@ const dino = document.getElementById("dino")
 const rock = document.getElementById("rock")
 const score = document.getElementById("score")
 const highscore = document.getElementById("highscore-value")
+const background = document.getElementById("background")
+const startGameButton = document.getElementById("startGame")
+let gameLoopInteral = 0;
 
+startGameButton.addEventListener("click", (event) => {
+  startGame()
+})
 
-const restartGame = () => {
+const startGame = () => {
+  background.classList.add("bg-animation");
+  rock.classList.add("rock-animation");
+  startGameLoop()
+}
+
+const stopGame = () => {
+  background.classList.remove("bg-animation");
+  rock.classList.remove("rock-animation");
+  clearInterval(gameLoopInteral)
+}
+
+const resetScore = () => {
   const scoreNumber = parseInt(score.innerText)
   const highscoreNumber = parseInt(highscore.innerText)
   if (scoreNumber > highscoreNumber) {
@@ -12,19 +30,19 @@ const restartGame = () => {
   }
   score.innerText = 0
 }
- 
+
 const jump = () => {
   dino.classList.add("jump-animation")
-  setTimeout( () => { 
+  setTimeout(() => {
     dino.classList.remove("jump-animation")
-    }, 500)
+  }, 500)
 }
 
 const die = () => {
   dino.classList.add("dino-dies")
-  setTimeout( () => { 
+  setTimeout(() => {
     dino.classList.remove("dino-dies")
-    }, 500)
+  }, 500)
 }
 
 document.addEventListener('keypress', (event) => {
@@ -33,22 +51,28 @@ document.addEventListener('keypress', (event) => {
   }
 })
 
-window.setInterval(() => {
-  const dinoTop = parseInt(window.getComputedStyle(dino)
-    .getPropertyValue('top'))
-  const rockLeft = parseInt(window.getComputedStyle(rock)
-    .getPropertyValue('left'))
+const startGameLoop = () => {
+  gameLoopInteral = window.setInterval(() => {
+    const dinoTop = parseInt(window.getComputedStyle(dino)
+      .getPropertyValue('top'))
+    const rockLeft = parseInt(window.getComputedStyle(rock)
+      .getPropertyValue('left'))
 
-  score.innerText = Number(score.innerText) + 1
+    score.innerText = Number(score.innerText) + 1
 
-  if (rockLeft < 0) {
-    rock.style.display = 'none'
-  } else {
-    rock.style.display = ''
-  }
+    if (rockLeft < 0) {
+      rock.style.display = 'none'
+    } else {
+      rock.style.display = ''
+    }
 
-  if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
-  die()
-    restartGame()
-  }
-}, 50)
+    if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
+      die()
+      resetScore()
+      stopGame()
+    }
+  }, 50)
+
+}
+
+
